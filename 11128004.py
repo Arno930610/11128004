7d245334b5bcedffe9a77f6ff33780cc75650a5f
Arno930610 <166378620+Arno930610@users.noreply.github.com>
2024-04-08 17:08:05 +0800

    Add files via upload

---
+++ 11128004.py
@@ -0,0 +1,10 @@
+import math
+score = {'A':84,'B':90,'C':75,'D':82}
+print("最高分為：")
+print(max(score.values()))
+print("最低分為：")
+print(min(score.values()))
+print("人數為：")
+print(len(score))
+print("平均分：")
+print(sum(score.values())/len(score))
